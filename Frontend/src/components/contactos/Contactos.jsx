9f30b1ce5f991fceca859a0eae0d60a68e060be5
Janice Chen <janicechen31997@hotmail.com>
2023-03-29 09:03:19 -0500

    commit3

--- Frontend/src/components/contactos/Contactos.jsx
+++ Frontend/src/components/contactos/Contactos.jsx
@@ -25,9 +25,6 @@ function Contactos (){
           <a href="#"><i className="fa fa-pinterest" /></a>
           <a href="#"><i className="fa fa-facebook" /></a>
           <a href="#"><i className="fa fa-twitter" /></a>
-          <a href="#"><i className="fa fa-dribbble" /></a>
-          <a href="#"><i className="fa fa-behance" /></a>
-          <a href="#"><i className="fa fa-linkedin" /></a>
         </div>
       </div>
       <div className="col-lg-8">
